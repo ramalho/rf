2ac912c30647234d29641ad20bc6b4b6cc8ab9ef
Luciano Ramalho <luciano@ramalho.org>
2019-05-12 00:21:31 -0300

    implemented count of characters found

--- elixir/rf.exs
+++ elixir/rf.exs
@@ -1,22 +1,28 @@
 #! /usr/bin/env elixir
 
 defmodule RuneFinder do
-  defp find(query_words) do
-    data_path = Path.join(__DIR__, "UnicodeData.txt")
-
-    Enum.each(File.stream!(data_path), fn line ->
-      {code, name, name_words} = parse(line)
-
-      if MapSet.subset?(query_words, name_words) do
-        rune = <<String.to_integer(code, 16)::utf8>>
-        IO.puts("U+#{code}\t#{rune}\t#{name}")
-      end
-    end)
-  end
-
   defp parse(line) do
     [code, name | _] = String.split(line, ";")
     {code, name, tokenize(name)}
+  end
+
+  defp find(query_words) do
+    count =
+      Path.join(__DIR__, "UnicodeData.txt")
+      |> File.stream!()
+      |> Enum.reduce(0, fn line, count ->
+        {code, name, name_words} = parse(line)
+
+        if MapSet.subset?(query_words, name_words) do
+          rune = <<String.to_integer(code, 16)::utf8>>
+          IO.puts("U+#{code}\t#{rune}\t#{name}")
+          count + 1
+        else
+          count
+        end
+      end)
+
+    IO.puts("(#{count} found)")
   end
 
   defp tokenize(text) do
