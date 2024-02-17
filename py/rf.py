0c4ae63c62d216fb377a337f6b012872416e6d24
Luciano Ramalho <luciano@ramalho.org>
2024-02-17 19:31:33 -0300

    split hyphenated queries and names

--- py/rf.py
+++ py/rf.py
@@ -2,27 +2,31 @@
 import sys
 import unicodedata
 
-FIRST, LAST = ord(" "), sys.maxunicode
+FIRST, LAST = ord(' '), sys.maxunicode
 
 
-def find(*words, first=FIRST, last=LAST):
-    query = {w.upper() for w in words}
+def tokenize(text):
+    return text.replace('-', ' ').split()
+
+
+def find(text, first=FIRST, last=LAST):
+    query = set(tokenize(text.upper()))
     count = 0
     for code in range(first, last + 1):
         char = chr(code)
-        name = unicodedata.name(char, "")
-        if name and query.issubset(name.split()):
-            print(f"U+{code:04X}\t{char}\t{name}")
+        name = unicodedata.name(char, '')
+        if name and query.issubset(tokenize(name)):
+            print(f'U+{code:04X}\t{char}\t{name}')
             count += 1
-    print(f"({count} found)")
+    print(f'({count} found)')
 
 
-def main(words):
-    if words:
-        find(*words)
+def main(args):
+    if args:
+        find(' '.join(args))
     else:
-        print("Please provide words to find.")
+        print('Please provide words to find.')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main(sys.argv[1:])
--- py/rf_tests.rst
+++ py/rf_tests.rst
@@ -18,17 +18,33 @@ Import functions for testing::
 
 Test ``find`` with single result::
 
-    >>> find("sign", "registered")  # doctest:+NORMALIZE_WHITESPACE
+    >>> find("sign registered")  # doctest:+NORMALIZE_WHITESPACE
     U+00AE  ®   REGISTERED SIGN
     (1 found)
 
 
 Test ``find`` with two results::
 
-    >>> find("chess", "queen", first=0x2654, last=0x2660)  # doctest:+NORMALIZE_WHITESPACE
+    >>> find("chess queen", first=0x2654, last=0x2660)  # doctest:+NORMALIZE_WHITESPACE
     U+2655	♕	WHITE CHESS QUEEN
     U+265B	♛	BLACK CHESS QUEEN
     (2 found)
+
+Test ``find`` with hyphenated names::
+
+    >>> find('hyphen', last=0xFF)  # doctest:+NORMALIZE_WHITESPACE
+    U+002D	-	HYPHEN-MINUS
+    U+00AD	­	SOFT HYPHEN
+    (2 found)
+
+
+Test ``find`` with hyphenated query::
+
+    >>> find('minus-hyphen', last=0xFFFF)  # doctest:+NORMALIZE_WHITESPACE
+    U+002D	-	HYPHEN-MINUS
+    U+FE63	﹣	SMALL HYPHEN-MINUS
+    U+FF0D	－	FULLWIDTH HYPHEN-MINUS
+    (3 found)
 
 Test ``main`` with no words::
 
