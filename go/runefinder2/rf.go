90e7d5ccc30deace5d73b70a0bd7671335244d99
Luciano Ramalho <luciano@ramalho.org>
2025-09-26 18:33:01 -0300

    creating runefinder2

---
+++ go/runefinder2/rf.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"slices"
+	"strings"
+	"unicode"
+
+	"golang.org/x/text/unicode/runenames"
+)
+
+const default_first, default_last = ' ', unicode.MaxRune
+
+func tokenize(text string) []string {
+	return strings.Fields(strings.ReplaceAll(text, "-", " "))
+}
+
+// containsAll reports whether list contains all items.
+func containsAll(list []string, items []string) bool {
+	for _, s := range items {
+		if !slices.Contains(list, s) {
+			return false
+		}
+	}
+	return true
+}
+
+func find(text string, firstLast ...rune) {
+	first, last := default_first, default_last
+	switch len(firstLast) {
+	case 0:
+		// done
+	case 1:
+		first = firstLast[0]
+	case 2:
+		first, last = firstLast[0], firstLast[1]
+	default:
+		panic("find: firstLast must be 0, 1 or 2 runes")
+	}
+	words := tokenize(text)
+	query := []string{}
+	for _, word := range words {
+		query = append(query, strings.ToUpper(word))
+	}
+	count := 0
+	for char := first; char <= last; char++ {
+		name := runenames.Name(char)
+		if len(name) == 0 {
+			continue
+		}
+		if containsAll(tokenize(name), query) {
+			fmt.Printf("%U\t%c\t%v\n", char, char, name)
+			count++
+		}
+	}
+	fmt.Printf("(%d found)\n", count)
+}
+
+func main() {
+	if len(os.Args) > 1 {
+		find(strings.Join(os.Args[1:], " "))
+	} else {
+		fmt.Println("Please provide words to find.\nExample:")
+		fmt.Println(os.Args[0], "cat eyes")
+	}
+}
---
+++ go/runefinder2/rf_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func Example_find() {
+	find("cruzeiro")
+	// Output:
+	// U+20A2	₢	CRUZEIRO SIGN
+	// (1 found)
+}
+
+func Example_find_two_results() {
+	find("completion hexagram")
+	// Output:
+	// U+4DFE	䷾	HEXAGRAM FOR AFTER COMPLETION
+	// U+4DFF	䷿	HEXAGRAM FOR BEFORE COMPLETION
+	// (2 found)
+}
+
+func Example_find_hyphenated_name() {
+	find("hyphen", ' ', '\x80')
+	// Output:
+	// U+002D	-	HYPHEN-MINUS
+	// (1 found)
+}
+
+func Example() {
+	oldArgs := os.Args
+	defer func() { os.Args = oldArgs }()
+	os.Args = []string{"<executable-name>", "apl", "tilde"}
+	main()
+	// Output:
+	// U+2368	⍨	APL FUNCTIONAL SYMBOL TILDE DIAERESIS
+	// U+236B	⍫	APL FUNCTIONAL SYMBOL DEL TILDE
+	// U+236D	⍭	APL FUNCTIONAL SYMBOL STILE TILDE
+	// U+2371	⍱	APL FUNCTIONAL SYMBOL DOWN CARET TILDE
+	// U+2372	⍲	APL FUNCTIONAL SYMBOL UP CARET TILDE
+	// (5 found)
+}
+
+func Example_no_args() {
+	oldArgs := os.Args
+	defer func() { os.Args = oldArgs }()
+	os.Args = []string{"<executable-name>"}
+	main()
+	// Output:
+	// Please provide words to find.
+	// Example:
+	// <executable-name> cat eyes
+}
+
+func TestContainsAll(t *testing.T) {
+	testCases := []struct {
+		list  []string
+		items []string
+		want  bool
+		name  string
+	}{
+		{[]string{}, []string{"B"}, false, "1 item, empty list"},
+		{[]string{"A"}, []string{"B"}, false, "1 item, not found"},
+		{[]string{"B"}, []string{"B"}, true, "1 item, found"},
+		{[]string{"A", "B"}, []string{"B"}, true, "1 item, found last"},
+		{[]string{}, []string{"B", "A"}, false, "2 items, empty list"},
+		{[]string{"A"}, []string{"B", "A"}, false, "2 items, B not found"},
+		{[]string{"A"}, []string{"B", "A"}, false, "2 items, A not found"},
+		{[]string{"A", "B"}, []string{"B", "A"}, true, "2 items, found"},
+		{[]string{"A", "B", "C"}, []string{"B", "A"}, true, "2 items, found with extra"},
+		{[]string{}, []string{}, true, "0 item, empty list"},
+		{[]string{"A"}, []string{}, true, "0 item, any list"},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := containsAll(tc.list, tc.items)
+			if got != tc.want {
+				t.Errorf("got %v; want %v", got, tc.want)
+			}
+		})
+	}
+}
